226c0777a3311b7c370d4ad8f4e622e07f9e1658
winkelco <57962651+winkelco@users.noreply.github.com>
2019-11-19 14:26:19 -0500

    Update MercadoLibreUser.php

--- src/Socialite/MercadoLibreUser.php
+++ src/Socialite/MercadoLibreUser.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-namespace WinkelCo\LaravelMercadoLibre\Socialite;
+namespace WinkelCo\MercadoLibre\Socialite;
 
 use Zephia\MercadoLibre\Entity\User;
 
